805ed8e322faca0294d3ffebfcec225dfb67a1f6
prashantpandey9 <prashantpandey94551@gmail.com>
2020-11-13 14:45:02 +0530

    added masonry layout

--- main.js
+++ main.js
@@ -13,70 +13,21 @@ menuBtn.addEventListener('click',() => {
 });
 
 
+var div = document.createElement('div');
 
-// let scene, camera, renderer, stars, starGeo;
-
-// function init() {
-//   scene = new THREE.Scene();
-
-//   camera = new THREE.PerspectiveCamera(
-//     60,
-//     window.innerWidth / window.innerHeight,
-//     1,
-//     1000
-//   );
-//   camera.position.z =1;
-//   camera.rotation.x = Math.PI / 2;
-
-//   renderer = new THREE.WebGLRenderer();
-//   renderer.setSize(window.innerWidth, window.innerHeight);
-//   document.body.appendChild(renderer.domElement);
-
-//   starGeo = new THREE.Geometry();
-//   for (let i = 0; i < 6000; i++) {
-//     star = new THREE.Vector3(
-//       Math.random() * 600 - 300,
-//       Math.random() * 600 - 300,
-//       Math.random() * 600 - 300
-//     );
-//     star.velocity = 0;
-//     star.acceleration = 0.01;
-//     starGeo.vertices.push(star);
-//   }
-
-//   let sprite = new THREE.TextureLoader().load("https://i.ibb.co/hH0YbHW/stars.png");
-//   let starMaterial = new THREE.PointsMaterial({
-//     color: 0xaaaaaa,
-//     size: 0.9,
-//     map: sprite,
-//   });
-
-//   stars = new THREE.Points(starGeo, starMaterial);
-//   scene.add(stars);
-
-//   window.addEventListener("resize", onWindowResize, false);
-
-//   animate();
-// }
-// function onWindowResize() {
-//   camera.aspect = window.innerWidth / window.innerHeight;
-//   camera.updateProjectionMatrix();
-//   renderer.setSize(window.innerWidth, window.innerHeight);
-// }
-// function animate() {
-//   starGeo.vertices.forEach((p) => {
-//     p.velocity += p.acceleration;
-//     p.y -= p.velocity;
-
-//     if (p.y < -200) {
-//       p.y = 200;
-//       p.velocity = 0;
-//     }
-//   });
-//   starGeo.verticesNeedUpdate = true;
-//   stars.rotation.y += 0.002;
-
-//   renderer.render(scene, camera);
-//   requestAnimationFrame(animate);
-// }
-// init();
+div.innerHTML = `
+        <div class="grid">
+            <img src=${"./gallery/img1.jpg"}>
+            <div class="grid__body">
+            <div class="relative">
+                <a class="grid__link" target="_blank" href="/" ></a>
+                <h1 class="grid__title">Title 1</h1>
+                <p class="grid__author">Mario Rossi</p>
+            </div>
+            <div class="mt-auto" >
+                <span class="grid__tag">#tag1</span>
+            </div>
+            </div>
+        </div>
+`;
+document.getElementById('Gallery').appendChild(div);
