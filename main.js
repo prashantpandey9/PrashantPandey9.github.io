fb996b6acd2e93a18e4204a0670eb3db6ab98f6e
prashantpandey9 <prashantpandey94551@gmail.com>
2020-11-13 15:35:23 +0530

    added gallery clear

--- main.js
+++ main.js
@@ -14,22 +14,25 @@ menuBtn.addEventListener('click',() => {
 
 
 var div = document.createElement('div');
-for(let s=1; s<53; s++){
+for(let s=1; s<52; s++){
     div.innerHTML += `
         <div class="grid">
             <img src=${`gallery/${s}.jpg`}>
-            <div class="grid__body">
-            <div class="relative">
-                <a class="grid__link" target="_blank" href="/" ></a>
-                <h1 class="grid__title">Title 1</h1>
-                <p class="grid__author">Mario Rossi</p>
-            </div>
-            <div class="mt-auto" >
-                <span class="grid__tag">#tag1</span>
-            </div>
-            </div>
         </div>
     `;
 }
 
 document.getElementById('Gallery').appendChild(div);
+
+// `
+// <div class="grid__body">
+//     <div class="relative">
+//         <a class="grid__link" target="_blank" href="/" ></a>
+//         <h1 class="grid__title">Title 1</h1>
+//         <p class="grid__author">Mario Rossi</p>
+//     </div>
+//     <div class="mt-auto" >
+//         <span class="grid__tag">#tag1</span>
+//     </div>
+// </div>
+// `
